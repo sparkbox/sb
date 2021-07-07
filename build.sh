4a6b51f9d32fd502533e8d1d29f8f5f7ba4d5161
Adam Simpson <adam@heysparkbox.com>
2021-07-07 15:17:50 -0400

    wip: workflow file

--- build.sh
+++ build.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 
-GIT_TAG=$(git tag | tail -1); GOOS=darwin GOARCH=arm64 go build -o arm64-macos-sb -a -ldflags="-X 'sb/cmd.AppVersion=$GIT_TAG'"
+# GOOS=darwin GOARCH=arm64 go build -o arm64-macos-sb -a -ldflags="-X 'sb/cmd.AppVersion=$GIT_TAG'"
 
-GIT_TAG=$(git tag | tail -1); GOOS=darwin GOARCH=amd64 go build -o amd64-macos-sb -a -ldflags="-X 'sb/cmd.AppVersion=$GIT_TAG'"
+# GOOS=darwin GOARCH=amd64 go build -o amd64-macos-sb -a -ldflags="-X 'sb/cmd.AppVersion=$GIT_TAG'"
 
-GIT_TAG=$(git tag | tail -1); GOOS=linux GOARCH=amd64 go build -o amd64-linux-sb -a -ldflags="-X 'sb/cmd.AppVersion=$GIT_TAG'"
+# GOOS=linux GOARCH=amd64 go build -o amd64-linux-sb -a -ldflags="-X 'sb/cmd.AppVersion=$GIT_TAG'"
 
-zip arm64-macos-sb.zip arm64-macos-sb
+# zip arm64-macos-sb.zip arm64-macos-sb
 
-zip amd64-macos-sb.zip amd64-macos-sb
+# zip amd64-macos-sb.zip amd64-macos-sb
 
-zip amd64-linux-sb.zip amd64-linux-sb
+# zip amd64-linux-sb.zip amd64-linux-sb
 
-rm *-sb
+# curl -d "https://api.github.com/repos/sparkbox/sb/releases/{release_id}/assets"
+
+cat "${GITHUB_EVENT_PATH}"
